3fc482661774720c0eb0b4eefde051148e77728f
Sviatoslav <sviatoslav.tkachuk11@gmail.com>
2023-10-30 17:50:04 +0200

    Add prioritets to task 1

--- task1/element.py
+++ task1/element.py
@@ -20,6 +20,7 @@ class Element:
 
         self.probabilities = None
         self.blocked = None
+        self.priorities = None
 
     def in_act(self, t_curr):
         pass
@@ -41,19 +42,43 @@ class Element:
     def do_statistics(self, delta):
         pass
 
-    def set_next_elements(self, next_elements, probabilities=None, blocked=None):
+    def set_next_elements(self, next_elements, probabilities=None, priorities=None, blocked=None):
         self.next_elements = next_elements
         self.probabilities = probabilities
         self.blocked = blocked
+        self.priorities = priorities
 
     def get_next(self):
         if self.probabilities:
-            n = random.random()
-            r = 0
-            for i, p in enumerate(self.probabilities):
-                r += p
-                if n < r:
-                    return self.next_elements[i]
+            while True:
+                n = random.random()
+                r = 0
+                for i, p in enumerate(self.probabilities):
+                    r += p
+                    if n < r and (self.blocked is None or not self.blocked[i]):
+                        return self.next_elements[i]
+
+        if self.priorities:
+            if self.blocked is not None:
+                elements = []
+                for i, _ in enumerate(self.next_elements):
+                    if not self.blocked[i]:
+                        elements.append((self.next_elements[i], self.priorities[i]))
+            else:
+                elements = list(zip(self.next_elements, self.priorities))
+            random.shuffle(elements)
+            max_prior_el = elements[0]
+            for el in elements[1:]:
+                if el[1] > max_prior_el[1]:
+                    max_prior_el = el
+            return max_prior_el[0]
+
+        if self.blocked is not None:
+            elements = random.choice(self.next_elements)
+            for i, _ in self.blocked:
+                if self.blocked[i]:
+                    del elements[i]
+            return random.choice(self.next_elements)
         return random.choice(self.next_elements)
 
     def get_delay(self):
--- task1/main.py
+++ task1/main.py
@@ -8,8 +8,9 @@ process2 = ElementProcess(delay=1, name="Process2", max_queue=5, count_of_worker
 process3 = ElementProcess(delay=1, name="Process3", max_queue=5, count_of_workers=1)
 
 create.set_next_elements([process1])
-process1.set_next_elements([process2, process1])
-process2.set_next_elements([process3])
+process1.set_next_elements([process2, process1, process3],
+                           priorities=[2, 3, 1], blocked=[False, True, False])
+process2.set_next_elements([process3, process2], probabilities=[0.2, 0.8])
 
 elements = [create, process1, process2, process3]
 model = Model(elements)
